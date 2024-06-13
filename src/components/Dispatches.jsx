5e4cf0244f7f40002e36c36f2f75acb7d1425761
ashortsleeves <ashortsleeves@gmail.com>
2024-06-13 11:52:41 -0400

    Dispatches styling

--- src/components/Dispatches.jsx
+++ src/components/Dispatches.jsx
@@ -3,6 +3,15 @@ import React, {useState, useEffect } from 'react';
 export default function NewsFeed() {
     const [dispatches, setDispatches] = useState([]);
 
+    function findFirstLowerCaseIndex(str) {
+        for (let i = 0; i < str.length; i++) {
+            if (str[i] !== str[i].toUpperCase()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     const fetchData = async () => {
         try {
             const dispatchesResponse = await fetch('https://raw.githubusercontent.com/ashortsleeves/heckdivers-json/main/dispatches.json');
@@ -21,15 +30,38 @@ export default function NewsFeed() {
     }, []);
 
     return (
-        <div className='dispatches'>
-            <h1>test</h1>
-            {dispatches.length > 0 ? (
-                dispatches.map((dispatch, index) => (
-                    <div className="dispatch" key={index}>
-                        <div dangerouslySetInnerHTML={{ __html: dispatch.message }} />
-                    </div>
-                ))
-            ) : ''}
-        </div>
+        <>
+            <div className='dispatches'>
+                { dispatches.length > 0 ? (
+                    dispatches.slice(0, 5).map((dispatch, index) => {
+                        const dispatchUnHTML = dispatch.message.replace(/<[^>]*>/g, '');
+
+                        const dispatchLastCapsIndex = findFirstLowerCaseIndex(dispatchUnHTML);
+
+                        const dispatchTitle = dispatchUnHTML.slice(0, dispatchLastCapsIndex - 1).replace(/(\r\n|\n|\r)/gm,'');
+
+                        const dispatchClean = dispatchUnHTML.replace(dispatchTitle, '');
+
+                        const keyWords = /Vernen Wells|Wezen|Terminids|Automatons|Terminid|Automaton|Supercolony|Meridia/g;
+
+                        let dispatchContent = dispatchClean.replaceAll(keyWords, function(x) {
+                            return '<strong>' + x + '</strong>';
+                        });
+
+                        return (
+                            <div className="dispatch" key={index}>
+                                <h3 className={
+                                    dispatchTitle == 'NEW MAJOR ORDER' ? 'yellow' :
+                                    dispatchTitle == 'MAJOR ORDER WON' ? 'yellow' :
+                                     ''
+                                     }>{ dispatchTitle }</h3>
+                                <span>{ dispatch.published }</span>
+                                <p dangerouslySetInnerHTML={{ __html: dispatchContent }} />
+                            </div>
+                        );
+                    })
+                ) : ''}
+            </div>
+        </>
     );
 }
