f5125131bca4620cd1fd190439ca0f5df848d9d8
ashortsleeves <ashortsleeves@gmail.com>
2025-05-20 11:43:26 -0400

    remove previous body classes

--- src/assets/scripts/wallpaperEngine.js
+++ src/assets/scripts/wallpaperEngine.js
@@ -1,31 +1,37 @@
 window.wallpaperPropertyListener = {
-    applyUserProperties: function(properties) {
-        var bodyElement = document.body;
-        var map = document.getElementById("map");
+  applyUserProperties: function (properties) {
+    var bodyElement = document.body;
+    var map = document.getElementById("map");
 
-        if (properties.spin) {
-            var mySliderValue = properties.spin.value;
+    if (properties.spin) {
+      var mySliderValue = properties.spin.value;
 
-            if (mySliderValue) {
-              bodyElement.classList.add('rotate-active');
-            }
-            else {
-              bodyElement.classList.remove('rotate-active');
-            }
-        }
+      if (mySliderValue) {
+        bodyElement.classList.add('rotate-active');
+      }
+      else {
+        bodyElement.classList.remove('rotate-active');
+      }
+    }
 
-        if (properties.theme) {
-          var activeTheme = properties.theme.value;
+    if (properties.theme) {
+      var activeTheme = properties.theme.value;
 
-          if (activeTheme === 'basic') {
-            map.classList.add('basic');
-          }
-          else if(activeTheme === 'retro') {
-            map.classList.add('retro-active');
-          }
-          else {
-            map.classList.add('default');
-          }
-        }
-    },
+      if (activeTheme === 'basic') {
+        map.classList.add('basic');
+        map.classList.remove('retro-active');
+        map.classList.remove('default');
+      }
+      else if (activeTheme === 'retro') {
+        map.classList.add('retro-active');
+        map.classList.remove('basic');
+        map.classList.remove('default');
+      }
+      else {
+        map.classList.add('default');
+        map.classList.remove('retro-active');
+        map.classList.remove('basic');
+      }
+    }
+  },
 };
